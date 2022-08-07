5229f7b25348496d47499c2ff3ef9bd7161d3f7f
Adam Ainsworth <adam.ainsworth@gmail.com>
2022-08-07 13:19:30 +0100

    Add Linux version

--- setup-mac.sh
+++ setup-mac.sh
@@ -3,7 +3,7 @@ if [ -z "$1" ]
 then
     echo "No name given"
 else 
-    sed -i "s/EXAMPLE/$1/g" .env
-    sed -i "s/EXAMPLE/$1/g" .lando.yml
+    sed -i "" "s/EXAMPLE/$1/g" .env
+    sed -i "" "s/EXAMPLE/$1/g" .lando.yml
     rm -rf .git setup.sh setup-mac.sh
 fi
